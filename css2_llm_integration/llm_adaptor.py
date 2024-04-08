9c0a72f5b6460554213c0b6af848fb82b9b432fd
Navrin <navman360@gmail.com>
2024-04-08 12:58:12 +1000

    Add conversation awareness and update db-build to work with local instances
    Please note that your .env will need to be renamed to .env-docker if you are using docker to run the application!

--- css2_llm_integration/llm_adaptor.py
+++ css2_llm_integration/llm_adaptor.py
@@ -1,10 +1,12 @@
+import typing as t
 """
 Base class for an LLM adaptor.
 """
 
+HistoryMessage = t.TypedDict('HistoryMessage', {'role': str, 'content': str})
 
 class LLMAdaptor:
     name = "Base Adaptor"
 
-    async def do_query(self, prompt: str) -> str:
+    async def do_query(self, prompt: str, history: t.List[HistoryMessage]) -> str:
         raise NotImplementedError
