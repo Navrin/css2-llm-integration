6ba53cf65f6e23d6cb0d296495a5fd9edd3b9e60
Navrin <navman360@gmail.com>
2024-04-19 13:30:35 +1000

    patch: let batching run with history

--- css2_llm_integration/entry.py
+++ css2_llm_integration/entry.py
@@ -1,4 +1,6 @@
 import asyncio
+import typing
+
 import simplejson as json
 
 import dotenv
@@ -9,6 +11,7 @@ import psycopg2
 from itertools import chain
 
 from css2_llm_integration.api.api import ChatSession
+from css2_llm_integration.llm_adaptor import HistoryMessage
 from templates import *
 
 dotenv.load_dotenv(f"{os.environ['PROJECT_ROOT']}/.env")
@@ -19,13 +22,13 @@ db_conn =  psycopg2.connect(f"host={cfg['DB_HOST']} dbname={cfg['DB_NAME']} user
 
 
 
-# active_adaptor = adaptors.OpenAIAdaptor()
+active_adaptor = adaptors.OpenAIAdaptor()
 device = -1
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-if "active_adaptor" not in st.session_state:
-    st.session_state.active_adaptor = None
+# if "active_adaptor" not in st.session_state:
+#     st.session_state.active_adaptor = None
 
 if "set" not in st.session_state:
 
@@ -71,7 +74,7 @@ async def main():
             "Hermes-2-Pro-Mistral-7B": lambda: adaptors.HFAdaptor(model="NousResearch/Hermes-2-Pro-Mistral-7B", device=device),
         }
         selected_model = st.selectbox("Select a model", list(model_choices.keys()))
-        st.session_state.active_adaptor = model_choices[(selected_model if selected_model is not None else "ChatGPT API")]()
+        active_adaptor = model_choices[(selected_model if selected_model is not None else "ChatGPT API")]()
 
         with st.form(key="batch_prompts"):
             json_text = st.text_area("Enter batch prompt in json", placeholder='{\n "prompts": ["what are my best sellers", "what products need improvement", "what stores sell the most Arctic Freezes" ]\n } ')
@@ -80,20 +83,39 @@ async def main():
             if run:
                 batch = json.loads(json_text)
                 prompts = batch["prompts"]
-                session = ChatSession(model=st.session_state.active_adaptor, db=db_conn,
-                                      history=st.session_state.messages)
-                batch_response = []
-                for prompt in prompts:
-                    st.session_state.messages = []
-                    runner = session.run_prompt(prompt)
-                    res = []
-                    async for n in runner:
-                        res.append(n)
+                fake_history: typing.List[HistoryMessage] = []
 
-                    batch_response.append({
-                        "prompt": prompt,
-                        "response": res[-1]
-                    })
+                session = ChatSession(model=st.session_state.active_adaptor, db=db_conn,
+                                      history=fake_history)
+                batch_response = []
+                for prompt_in in prompts:
+                    run = []
+                    fake_history.clear()
+
+                    if type(prompt_in) is str:
+                        run.append(prompt_in)
+                    elif type(prompt_in) is list:
+                        run = prompt_in
+
+                    for prompt in run:
+                        fake_history.append({
+                            "role": "user",
+                            "content": prompt,
+                        })
+                        runner = session.run_prompt(prompt)
+                        res = []
+
+                        async for n in runner:
+                            fake_history.append({
+                                "role": "assistant",
+                                "content": n
+                            })
+                            res.append(n)
+
+                        batch_response.append({
+                            "prompt": prompt,
+                            "response": res[-1]
+                        })
                 st.json(batch_response)
 
     with chat_col:
@@ -106,13 +128,13 @@ async def main():
                 "Hello. What is your query? I can answer questions about sales, or I can look at customer reviews for you.")
 
         prompter = st.chat_input("> What is your prompt?")
-        if prompt := prompter:
+        if prompt_in := prompter:
             with st.chat_message("user"):
-                st.write(prompt)
-            st.session_state.messages.append({"role": "user", "content": prompt, "ignore": True})
+                st.write(prompt_in)
+            st.session_state.messages.append({"role": "user", "content": prompt_in, "ignore": True})
 
-            session = ChatSession(model=st.session_state.active_adaptor, db=db_conn, history=st.session_state.messages)
-            runner = session.run_prompt(prompt)
+            session = ChatSession(model=active_adaptor, db=db_conn, history=st.session_state.messages)
+            runner = session.run_prompt(prompt_in)
 
             with st.chat_message("assistant"):
                 ai_msg = await anext(runner)
